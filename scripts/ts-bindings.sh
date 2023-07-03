fef8c95aa41840e9113295ebad0ae71f613942bd
damien <doute.d@gmail.com>
2023-07-03 13:22:21 -0400

    Adding bindings

--- scripts/ts-bindings.sh
+++ scripts/ts-bindings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-zits -i -i -i crates/path_explorer -o webcomponents/src/bindings/path_explorer.ts
+zits -i -i -i crates/path_explorer -o webcomponents/src/bindings/path-explorer.ts
 
 zits --default-zome-name zTasker -i playground/zomes/tasker -i playground/zomes/tasker_model -o playground/webapp/src/bindings/tasker.ts
